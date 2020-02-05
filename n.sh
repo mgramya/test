f9d4a0f4b7c39e75bc317f87ffdd6e2e709bd6c6
mgramya <ramyamg@tesinsights.com>
2020-02-05 14:13:06 +0530

    abd

--- n.sh
+++ n.sh
@@ -1,2 +1,19 @@
-#! /bin/bash
-echo "hello world"
+#¡ /bin/bash
+ls ./*.txt
+echo "enter file name: "
+read n
+sum=0;
+
+a=(`egrep '\d+' $n`)
+if [[ ${#a[*]} ]]
+	then 
+			echo "Your file is: $n"
+			 for i in ${a[*]}
+             do
+               (( sum =sum + $i )) 
+             done
+          echo "The result is: $sum"
+else
+      echo "Invalid file" 
+  fi
+
